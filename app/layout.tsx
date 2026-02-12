8de007654b76fa637c863e9f29e6ecb8bf667bb7
nouamanecodes <nouamane.benbrahim@protonmail.com>
2026-02-12 17:12:39 -0500

    refactor: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,9 +89,9 @@ export default function RootLayout({
         <DarkModeProvider>
           {children}
         </DarkModeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
-      <Analytics />
     </html>
   )
 }
